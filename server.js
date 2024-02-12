c9d6ff0c075d1eec14ad19cd123543c131f48300
꧁Voltage꧂ <f2v74yt@gmail.com>
2024-02-12 11:42:10 -0600

    Update server.js

--- server.js
+++ server.js
@@ -49,24 +49,43 @@ app.post('/generate-links', async (req, res) => {
 // Serve static files (including the generated file for download)
 app.use('/download', express.static('uploads'));
 
+
+// Function to generate a random string
+function generateRandomString(length) {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
 // Function to generate links
 async function generateLinks(quantity) {
     const links = [];
   
     for (let i = 0; i < quantity; i++) {
-      const partnerUserId = '15ddc8b179e57bfc769afe550245833ef836bf303c076ca96947e0954f0766fe';
+      // Generate a random string of length 64
+    const partnerUserId = generateRandomString(64);
   
       // Make a request to the external API using axios
       const apiResponse = await axios.post('https://api.discord.gx.games/v1/direct-fulfillment', {
         partnerUserId,
       }, {
         headers: {
-          'Content-Type': 'application/json',
+          'authority': 'api.discord.gx.games',
+          'accept': '*/*',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          'origin': 'https://www.opera.com',
+          'referer': 'https://www.opera.com/',
           'sec-ch-ua': '"Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"',
           'sec-ch-ua-mobile': '?0',
-          'sec-ch-ua-platform': '"macOS"',
-          'Referer': 'https://www.opera.com/',
-          'Referrer-Policy': 'strict-origin-when-cross-origin',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0'
         },
       });
   
